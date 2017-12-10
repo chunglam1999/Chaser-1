450c34752dbbdbbc247baa56aa9213d092327dfe
chunglam1999 <chunglam1999@gmail.com>
2017-12-09 21:58:14 -0800

    create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/4.8.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDHcU4s9kWpITJFMzUmJMtJvXCSSlnjPXQ",
+    authDomain: "chaser-37afa.firebaseapp.com",
+    databaseURL: "https://chaser-37afa.firebaseio.com",
+    projectId: "chaser-37afa",
+    storageBucket: "",
+    messagingSenderId: "984771377533"
+  };
+  firebase.initializeApp(config);
+</script>
